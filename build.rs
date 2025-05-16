2d734e0cab0ce87c0449319709eb68c27f29a1d9
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-05-16 12:31:07 +0200

    dont run codegen

--- build.rs
+++ build.rs
@@ -4,44 +4,44 @@
 //! and generates corresponding Rust code in src/versions/.
 //! The code is only regenerated if the specification files have changed.
 
-use mdmodels::prelude::{DataModel, Templates};
-use std::{fs, path::PathBuf};
+// use mdmodels::prelude::{DataModel, Templates};
+// use std::{fs, path::PathBuf};
 
 fn main() {
-    // Tell cargo to rerun this script only if something in specs/specifications changes
-    println!("cargo:rerun-if-changed=specs/specifications");
+    // // Tell cargo to rerun this script only if something in specs/specifications changes
+    // println!("cargo:rerun-if-changed=specs/specifications");
 
-    // Parse the specs and generate Rust code
-    let pattern = "specs/specifications/*.md";
-    let files = glob::glob(pattern).expect("Failed to read glob pattern");
+    // // Parse the specs and generate Rust code
+    // let pattern = "specs/specifications/*.md";
+    // let files = glob::glob(pattern).expect("Failed to read glob pattern");
 
-    for path in files.flatten() {
-        let fname = path
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .split('.')
-            .next()
-            .unwrap();
+    // for path in files.flatten() {
+    //     let fname = path
+    //         .file_name()
+    //         .unwrap()
+    //         .to_str()
+    //         .unwrap()
+    //         .split('.')
+    //         .next()
+    //         .unwrap();
 
-        // Parse the markdown file into a DataModel
-        let mut model = DataModel::from_markdown(&path).expect("Failed to parse markdown");
+    //     // Parse the markdown file into a DataModel
+    //     let mut model = DataModel::from_markdown(&path).expect("Failed to parse markdown");
 
-        // Generate compliant Rust code
-        let code = model
-            .convert_to(&Templates::Rust, None)
-            .expect("Failed to convert to Rust")
-            + "\n";
+    //     // Generate compliant Rust code
+    //     let code = model
+    //         .convert_to(&Templates::Rust, None)
+    //         .expect("Failed to convert to Rust")
+    //         + "\n";
 
-        // Create a new file with the same name as the markdown file
-        let out_path: PathBuf = format!("src/versions/{}.rs", fname).into();
+    //     // Create a new file with the same name as the markdown file
+    //     let out_path: PathBuf = format!("src/versions/{}.rs", fname).into();
 
-        // If the file doesn't exist, or if it exists but is different from the generated code, write the code to the file
-        if !out_path.exists() {
-            fs::write(out_path, code).expect("Failed to write file");
-        } else {
-            fs::write(out_path, code).expect("Failed to write file");
-        }
-    }
+    //     // If the file doesn't exist, or if it exists but is different from the generated code, write the code to the file
+    //     if !out_path.exists() {
+    //         fs::write(out_path, code).expect("Failed to write file");
+    //     } else {
+    //         fs::write(out_path, code).expect("Failed to write file");
+    //     }
+    // }
 }
