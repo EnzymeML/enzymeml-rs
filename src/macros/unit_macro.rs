b692a85c00d6b1dafaa07c38b81c902e928b1b8c
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-06-25 00:54:21 +0200

    refactor unit macro and add tests

--- src/macros/unit_macro.rs
+++ src/macros/unit_macro.rs
@@ -17,174 +17,171 @@
 //! - Handling unit scale and exponent calculations
 //! - Providing error checking for unknown prefixes or units
 
+/// Helper macro to create a single BaseUnit from prefix and unit tokens
 #[macro_export]
-macro_rules! unit {
-
-    ( 1 / $([ $num_prefix:tt $num_unit:ident ])+ ) => {
-        enzymeml::enzyme_ml::UnitDefinition {
-            // Convert all the prefixes and units to a string
-            id: None,
-            additional_properties: None,
-            name: format!(
-                "{}",
-                vec![
-                    $(
-                        stringify!($num_prefix),
-                        stringify!($num_unit)
-                    ),+
-                ].join("")
-            ).replace('_', "").into(),
-            // Parse units into base units
-            base_units: {
-                let mut units = vec![];
-                $(
-                    units.push(enzymeml::enzyme_ml::BaseUnit {
-                        scale: Some(
-                            enzymeml::macros::unit_maps::PREFIX_MAPPING
-                                .get(stringify!($num_prefix))
-                                .expect(
-                                    format!("Prefix {} not found", stringify!($num_prefix)).as_str()
-                                )
-                                .clone()
-                        ),
-                        kind: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().0,
-                        exponent: -1,
-                        multiplier: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().1,
-                        additional_properties: None,
-                    });
-                )+
-                units
-            },
+macro_rules! build_base_unit {
+    ($prefix:tt, $unit:ident, $exponent:expr) => {
+        $crate::prelude::BaseUnit {
+            scale: Some(
+                $crate::macros::unit_maps::PREFIX_MAPPING
+                    .get(stringify!($prefix))
+                    .expect(&format!("Prefix {} not found", stringify!($prefix)))
+                    .clone(),
+            ),
+            kind: $crate::macros::unit_maps::KIND_MAPPINGS
+                .get(stringify!($unit))
+                .expect(&format!("Unit {} not found", stringify!($unit)))
+                .clone()
+                .0,
+            exponent: $exponent,
+            multiplier: $crate::macros::unit_maps::KIND_MAPPINGS
+                .get(stringify!($unit))
+                .expect(&format!("Unit {} not found", stringify!($unit)))
+                .clone()
+                .1,
         }
     };
+}
 
-    ( $([ $num_prefix:tt $num_unit:ident ])+ / $([ $den_prefix:tt $den_unit:ident ])+ ) => {
-        enzymeml::enzyme_ml::UnitDefinition {
-            // Convert all the prefixes and units to a string
+/// Helper macro to build unit name from prefix/unit pairs
+#[macro_export]
+macro_rules! build_unit_name {
+    ($([$prefix:tt $unit:ident])+) => {
+        vec![
+            $(
+                stringify!($prefix),
+                stringify!($unit)
+            ),+
+        ].join("").replace('_', "")
+    };
+}
+
+/// Helper macro to collect base units with specified exponent
+#[macro_export]
+macro_rules! collect_base_units {
+    ($exponent:expr; $([$prefix:tt $unit:ident])+) => {
+        {
+            let mut units = vec![];
+            $(
+                units.push($crate::build_base_unit!($prefix, $unit, $exponent));
+            )+
+            units
+        }
+    };
+}
+
+/// Macro for creating inverse units (1 / unit)
+#[macro_export]
+macro_rules! inverse_unit {
+    ($([$prefix:tt $unit:ident])+) => {
+        $crate::prelude::UnitDefinition {
             id: None,
-            additional_properties: None,
+            name: format!("1 / {}", $crate::build_unit_name!($([$prefix $unit])+)).into(),
+            base_units: $crate::collect_base_units!(-1; $([$prefix $unit])+),
+        }
+    };
+}
+
+/// Macro for creating ratio units (numerator / denominator)
+#[macro_export]
+macro_rules! ratio_unit {
+    ($([$num_prefix:tt $num_unit:ident])+; $([$den_prefix:tt $den_unit:ident])+) => {
+        $crate::prelude::UnitDefinition {
+            id: None,
             name: format!(
                 "{} / {}",
-                vec![
-                    $(
-                        stringify!($num_prefix),
-                        stringify!($num_unit)
-                    ),+
-                ].join(""),
-                vec![
-                    $(
-                        stringify!($den_prefix),
-                        stringify!($den_unit)
-                    ),+
-                ].join("")
-            ).replace('_', "").into(),
-            // Parse units into base units
+                $crate::build_unit_name!($([$num_prefix $num_unit])+),
+                $crate::build_unit_name!($([$den_prefix $den_unit])+)
+            ).into(),
             base_units: {
-                let mut units = vec![];
-                $(
-                    units.push(enzymeml::enzyme_ml::BaseUnit {
-                        scale: Some(
-                            enzymeml::macros::unit_maps::PREFIX_MAPPING
-                                .get(stringify!($num_prefix))
-                                .expect(
-                                    format!("Prefix {} not found", stringify!($num_prefix)).as_str()
-                                )
-                                .clone()
-                        ),
-                        kind: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().0,
-                        exponent: 1,
-                        multiplier: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().1,
-                        additional_properties: None,
-                    });
-                )+
-                $(
-                    units.push(enzymeml::enzyme_ml::BaseUnit {
-                        scale: Some(
-                            enzymeml::macros::unit_maps::PREFIX_MAPPING
-                                .get(stringify!($den_prefix))
-                                .expect(
-                                    format!("Prefix {} not found", stringify!($den_prefix)).as_str()
-                                )
-                                .clone()
-                        ),
-                        kind: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($den_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($den_unit)).as_str()
-                                ).clone().0,
-                        exponent: -1,
-                        multiplier: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($den_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($den_unit)).as_str()
-                                ).clone().1,
-                        additional_properties: None,
-                    });
-                )+
+                let mut units = $crate::collect_base_units!(1; $([$num_prefix $num_unit])+);
+                units.extend($crate::collect_base_units!(-1; $([$den_prefix $den_unit])+));
                 units
             },
         }
+    };
+}
+
+/// Macro for creating simple units
+#[macro_export]
+macro_rules! simple_unit {
+    ($([$prefix:tt $unit:ident])+) => {
+        $crate::prelude::UnitDefinition {
+            id: None,
+            name: $crate::build_unit_name!($([$prefix $unit])+).into(),
+            base_units: $crate::collect_base_units!(1; $([$prefix $unit])+),
+        }
+    };
+}
+
+/// Main unit macro that delegates to appropriate sub-macros
+#[macro_export]
+macro_rules! unit {
+    // Inverse units: 1 / [prefix unit]+
+    (1 / $([$num_prefix:tt $num_unit:ident])+) => {
+        $crate::inverse_unit!($([$num_prefix $num_unit])+)
     };
 
-    ( $([ $num_prefix:tt $num_unit:ident ])+ ) => {
-        enzymeml::enzyme_ml::UnitDefinition {
-            // Convert all the prefixes and units to a string
-            id: None,
-            additional_properties: None,
-            name: format!(
-                "{}",
-                vec![
-                    $(
-                        stringify!($num_prefix),
-                        stringify!($num_unit)
-                    ),+
-                ].join("")
-            ).replace('_', "").into(),
-            // Parse units into base units
-            base_units: {
-                let mut units = vec![];
-                $(
-                    units.push(enzymeml::enzyme_ml::BaseUnit {
-                        scale: Some(
-                            enzymeml::macros::unit_maps::PREFIX_MAPPING
-                                .get(stringify!($num_prefix))
-                                .expect(
-                                    format!("Prefix {} not found", stringify!($num_prefix)).as_str()
-                                )
-                                .clone()
-                        ),
-                        kind: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().0,
-                        exponent: 1,
-                        multiplier: enzymeml::macros::unit_maps::KIND_MAPPINGS
-                                .get(stringify!($num_unit))
-                                .expect(
-                                    format!("Unit {} not found", stringify!($num_unit)).as_str()
-                                ).clone().1,
-                        additional_properties: None,
-                    });
-                )+
-                units
-            },
-        }
+    // Ratio units: [prefix unit]+ / [prefix unit]+
+    ($([$num_prefix:tt $num_unit:ident])+ / $([$den_prefix:tt $den_unit:ident])+) => {
+        $crate::ratio_unit!($([$num_prefix $num_unit])+; $([$den_prefix $den_unit])+)
     };
+
+    // Simple units: [prefix unit]+
+    ($([$num_prefix:tt $num_unit:ident])+) => {
+        $crate::simple_unit!($([$num_prefix $num_unit])+)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{prelude::UnitType, unit};
+
+    #[test]
+    fn test_unit_ratio_macro() {
+        let unit = unit!([_ mole] / [_ liter]);
+        assert_eq!(unit.name, Some("mole / liter".to_string()));
+        assert_eq!(unit.base_units.len(), 2);
+        assert_eq!(unit.base_units[0].kind, UnitType::Mole);
+        assert_eq!(unit.base_units[0].exponent, 1);
+        assert_eq!(unit.base_units[0].multiplier, None);
+        assert_eq!(unit.base_units[1].kind, UnitType::Litre);
+        assert_eq!(unit.base_units[1].exponent, -1);
+        assert_eq!(unit.base_units[1].multiplier, None);
+    }
+
+    #[test]
+    fn test_unit_ratio_macro_with_prefix() {
+        let unit = unit!([m mol] / [_ liter]);
+        assert_eq!(unit.name, Some("mmol / liter".to_string()));
+        assert_eq!(unit.base_units.len(), 2);
+        assert_eq!(unit.base_units[0].kind, UnitType::Mole);
+        assert_eq!(unit.base_units[0].scale, Some(-3.0));
+        assert_eq!(unit.base_units[0].exponent, 1);
+        assert_eq!(unit.base_units[0].multiplier, None);
+        assert_eq!(unit.base_units[1].kind, UnitType::Litre);
+        assert_eq!(unit.base_units[1].exponent, -1);
+        assert_eq!(unit.base_units[1].multiplier, None);
+    }
+
+    #[test]
+    fn test_unit_macro_with_prefix() {
+        let unit = unit!([m mole]);
+        assert_eq!(unit.name, Some("mmole".to_string()));
+        assert_eq!(unit.base_units.len(), 1);
+        assert_eq!(unit.base_units[0].kind, UnitType::Mole);
+        assert_eq!(unit.base_units[0].scale, Some(-3.0));
+        assert_eq!(unit.base_units[0].exponent, 1);
+    }
+
+    #[test]
+    fn test_unit_macro_inverse() {
+        let unit = unit!(1 / [_ liter]);
+        assert_eq!(unit.name, Some("1 / liter".to_string()));
+        assert_eq!(unit.base_units.len(), 1);
+        assert_eq!(unit.base_units[0].kind, UnitType::Litre);
+        assert_eq!(unit.base_units[0].scale, Some(1.0));
+        assert_eq!(unit.base_units[0].exponent, -1);
+        assert_eq!(unit.base_units[0].multiplier, None);
+    }
 }
